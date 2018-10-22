b7dbffdb80438fb6bae5a79928f60dcb34407c61
igor <verycooleagle@gmail.com>
2018-10-23 00:36:30 +0300

    Ready

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+apm i atom-easy-jsdoc
+apm i platformio-ide-terminal
+apm i todo-show
+apm i advanced-open-file
+apm i standard-formatter
+apm install file-icons
+apm install emmet
+# need check
+apm install figments
+apm install minimap
+apm install atom-expose
+apm install highlight-selected
